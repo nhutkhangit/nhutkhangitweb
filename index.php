4846f8af2c837d29203a29088080113fa2eff362
Liên Nhựt Khang <kendy.lien@beyondedge.co>
2019-10-18 17:31:27 +0700

    yii core

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+echo 'abcxyz';
+//$db = @mysql_connect("ip-172-31-26-133","root","");
+//if(!$db)
+//{
+//	echo "Không kết nối được với CSDL.";
+//	exit();
+//}
+//else
+//	mysql_select_db("nhutkhangweb");
+//mysql_set_charset("utf8");
+//
+//
+//function load_data()
+//{
+//	$result = array();
+//	$sql = mysql_query("SELECT * from user");
+//	while($mang = mysql_fetch_array($sql))
+//	{
+//		$result[] = $mang;
+//	}
+//	return $result;
+//}
+//$arrs=load_data();
+//var_dump($arrs);
+//
+//echo 'abcxyz';
+//?>
+<!---->
---
+++ models/Hello.php
@@ -0,0 +1,17 @@
+<?php
+
+	namespace app\models;
+	use yii\base\Model;
+	class Hello extends Model{
+		public $username;
+		public $email;
+
+		public function setUser($user, $pass){
+			$this->username = $user;
+			$this -> email = $pass;
+
+		}
+
+	}
+
+?>
