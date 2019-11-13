0d4a427f04f9289b16582d0d09c3a6c1ee7b4e35
Kendy Liên <kendy.lien@beyondedge.co>
2019-11-13 23:24:34 +0700

    update index

--- views/site/about.php
+++ views/site/about.php
@@ -10,21 +10,21 @@
 //	'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
 //]);
 // $this is the view object currently being used
-echo Breadcrumbs::widget([
-	'itemTemplate' => "<li><i>{link}</i></li>\n", // template for all links
-	'links'        => [
-		[
-			'label'    => 'Post Category',
-			'url'      => ['post-category/view', 'id' => 10],
-			'template' => "<li><b>{link}</b></li>\n", // template for this link only
-		],
-		[
-			'label' => 'Sample Post',
-			'url'   => ['post/edit',
-            'id' => 1]],
-		    'Edit',
-	],
-]);
+//echo Breadcrumbs::widget([
+//	'itemTemplate' => "<li><i>{link}</i></li>\n", // template for all links
+//	'links'        => [
+//		[
+//			'label'    => 'Post Category',
+//			'url'      => ['post-category/view', 'id' => 10],
+//			'template' => "<li><b>{link}</b></li>\n", // template for this link only
+//		],
+//		[
+//			'label' => 'Sample Post',
+//			'url'   => ['post/edit',
+//            'id' => 1]],
+//		    'Edit',
+//	],
+//]);
 ?>
 <div class="site-about">
     <div class="row">
