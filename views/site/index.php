c142088345912127062e167becc54bcfd7ba31da
Kendy Liên <kendy.lien@beyondedge.co>
2019-11-13 22:04:38 +0700

    update index

--- views/site/index.php
+++ views/site/index.php
@@ -37,9 +37,9 @@ $this->title = Yii::$app->name;
 
     </carousel-3d>
 </div>
-<script src='//production-assets.codepen.io/assets/common/stopExecutionOnTimeout-b2a7b3fe212eaa732349046d8416e00a9dec26eb7fd347590fbced3ab38af52e.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.7/vue.js'></script>
-<script src='https://rawgit.com/Wlada/vue-carousel-3d/master/dist/vue-carousel-3d.min.js'></script>
+<!--<script src='//production-assets.codepen.io/assets/common/stopExecutionOnTimeout-b2a7b3fe212eaa732349046d8416e00a9dec26eb7fd347590fbced3ab38af52e.js'></script>-->
+<!--<script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.7/vue.js'></script>-->
+<!--<script src='https://rawgit.com/Wlada/vue-carousel-3d/master/dist/vue-carousel-3d.min.js'></script>-->
 <script>new Vue({
     el: '#carousel3d',
     data: {
